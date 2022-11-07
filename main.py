9124c6b45a0dc076ac0511ce5f8f758927140a8c
Michal Kucera <mikucera@centrum.cz>
2022-11-07 17:56:11 +0100

    Přidána možnost čtení/zápisu z/do souboru.

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from rotator import word_rotator_core as r_func
 
-
 print(r_func(input("Enter a sentence or a word: ")))
+
 
 # TODO throwing exception in word_rotator_core() method
 # TODO capital letter detector
 # TODO cyclic input from the CLI (prompt: ri| )
 # TODO redirect i/o from/to file.txt
+
+with open("input_test_files.txt", encoding='utf-8') as data_rotator:
+    data_results = r_func(data_rotator.read())
+
+with open("output_file.txt", 'w', encoding='utf-8') as output:
+    output.write(data_results)
+
